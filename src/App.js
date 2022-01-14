4921394b9394d0ceb0a04ec5101b8dd6ebcb4131
HenriqueSerres <henriqueserres@gmail.com>
2022-01-14 18:06:14 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
-import Loguin from './pages/Login';
+import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
@@ -18,7 +18,7 @@ class App extends React.Component {
           <Route exact path="/search" component={ Search } />
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
-          <Route exact path="/" component={ Loguin } />
+          <Route exact path="/" component={ Login } />
           <Route exact path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import Loading from '../componets/Loading';
+import { createUser } from '../services/userAPI';
 
-export default class Loguin extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">Login</div>
-    );
+export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+      userName: '',
+      loading: false,
+      redirect: false,
+    };
   }
+
+ handleChange = (e) => {
+   const { isDisabled } = this.state;
+   const inputName = e.target.value;
+   const TRES = 3;
+   if (inputName !== undefined) {
+     this.setState({
+       isDisabled: isDisabled && inputName.length < TRES,
+       userName: inputName,
+     });
+   }
+ }
+
+ render() {
+   const { userName, isDisabled, loading, redirect } = this.state;
+
+   return (
+     <div data-testid="page-login">
+       <form>
+         <label htmlFor="login-name-input">
+           User Name
+           <input
+             type="text"
+             name="userName"
+             value={ userName }
+             onChange={ this.handleChange }
+             data-testid="login-name-input"
+           />
+         </label>
+         <button
+           type="button"
+           disabled={ isDisabled }
+           onClick={ async () => {
+             this.setState({ loading: true });
+             await createUser({ name: userName });
+             this.setState({ redirect: true });
+           } }
+           data-testid="login-submit-button"
+         >
+           Entrar
+         </button>
+         {loading && <Loading />}
+         {redirect && <Redirect to="/search" />}
+       </form>
+     </div>
+   );
+ }
 }
