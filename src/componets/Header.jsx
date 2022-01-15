e0b49659948ff565c65be21d678cc64e1ccc9588
HenriqueSerres <henriqueserres@gmail.com>
2022-01-15 16:46:49 -0300

    requisito 4 e 5

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import * as userAPI from '../services/userAPI';
 import Loading from './Loading';
 
@@ -8,6 +9,8 @@ export default class Header extends Component {
     this.state = {
       loading: false,
       names: '',
+      searchInput: '',
+      disabled: true,
     };
   }
 
@@ -21,13 +24,49 @@ fetchUserName = async () => {
   this.setState({ names, loading: false });
 }
 
+handleChange = (event) => {
+  const { name, value } = event.target;
+  this.setState({ [name]: value }, () => {
+    const { searchInput } = this.state
+    const DOIS = 2;
+    if (searchInput.length >= DOIS) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  });
+}
+
 render() {
-  const { loading, names } = this.state;
+  const { loading, names, searchInput, disabled } = this.state;
   return (
     <header data-testid="header-component">
       <span data-testid="header-user-name">
         {loading ? <Loading /> : names.name}
       </span>
+      <Link to="/search" data-testid="link-to-search">
+        Search
+      </Link>
+      <Link to="/favorites" data-testid="link-to-favorites">
+        Favorites
+      </Link>
+      <Link to="/profile" data-testid="link-to-profile">
+        Profile
+      </Link>
+      <input
+        data-testid="search-artist-input"
+        type="text"
+        name="searchInput"
+        value={ searchInput }
+        onChange={ this.handleChange }
+      />
+      <button
+        type="button"
+        data-testid="search-artist-button"
+        disabled={ disabled }
+      >
+        Pesquisar
+      </button>
     </header>
   );
 }
